7c18eb84d8cb1096595e77686ce0278f68796f20
AsmKhalid <130174468+asmkhalid111@users.noreply.github.com>
2024-10-22 20:18:45 +0600

    Initial Commit

---
+++ src/main/java/com/example/JavaProjectV0/JavaProjectV0Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaProjectV0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectV0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaProjectV0/JavaProjectV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaProjectV0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
